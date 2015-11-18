dd9100288971ddeac86698e5e64accbd194fa0cf
Henrique Nogueira <henrique.nogueira@ist.utl.pt>
2015-11-18 15:40:58 +0000

    Added gestor.c, main program

---
+++ gestor.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include"parkmap.h"
+
+int main(int argc, char* argv[]) {
+    Map *parkMap;
+    if(argc == 1) {
+        fprintf(stderr, "Usage ...\n");
+        return 1;
+    }
+    parkMap = mapInit(argv[1]);
+
+    return 0;
+}
