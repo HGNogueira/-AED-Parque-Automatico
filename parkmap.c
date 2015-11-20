d94065ae5d01e44ac4eb1f5ba01f488d265f81b6
Henrique Nogueira <henrique.nogueira@ist.utl.pt>
2015-11-20 22:21:37 +0000

    Created graph building functions: 	buildGraphs now builds both the car path graph and the peon path graph

--- parkmap.c
+++ parkmap.c
@@ -18,13 +18,23 @@
 #include"parkmap.h"
 #include"dbg.h"
 #include"point.h"
+#include"graphL.h"
 
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
-#define IDSIZE 64
-
+#define SIZE 256
+    /* toIndex - macro to convert coordinates into an 1 dimensional index
+     *
+     * a - column
+     * b - row
+     * c - floor
+     * A - num of columns
+     * B - num of rows 
+     * C - num of floors
+     */
+#define toIndex(a,b,c,A,B,C) a + A*(b) + A*B*c 
 
 
 /*
@@ -44,6 +54,8 @@ struct _map{
     char ***mapRep; /* table of matrices to represent multiple floor map */
     Point **accessPoints; /* table of map access points */
     Point **entrancePoints; /* table of map entrance points */
+
+    GraphL *cGraph, *pGraph; /* car and pedestrian graphs */
 };
 
 
@@ -69,7 +81,7 @@ Map *mapInit(char *filename) {
     Map *parkMap; 
     int n, m, p;                             /* iteration variables */
     char auxChar, desc;                      /* auxiliary chars */
-    char ID[256], *auxString, skipLine[256]; /* auxiliary strings */
+    char ID[SIZE], *auxString, skipLine[SIZE]; /* auxiliary strings */
     int x, y, z;                             /* point coordinate values */
     int atE = 0, atA = 0;                    /* control variables for access 
                                                 and entrance tables */
@@ -83,7 +95,7 @@ Map *mapInit(char *filename) {
     fscanf(fp, "%d %d %d %d %d", &parkMap->N, &parkMap->M, &parkMap->P,
                 &parkMap->E, &parkMap->S);
 
-    fgets(skipLine, 256, fp); /* make file stream point to next line) */
+    fgets(skipLine, SIZE, fp); /* make file stream point to next line) */
     skipLine[0] = '\0';
 
     /* initializing representation matrices */
@@ -110,7 +122,7 @@ Map *mapInit(char *filename) {
             for(n = 0; n < parkMap->N; n++) {
                 parkMap->mapRep[n][m][p] = (char) fgetc(fp); 
             }
-            fgets(skipLine, 256, fp); /* make file stream point to next line) */
+            fgets(skipLine, SIZE, fp); /* make file stream point to next line) */
             skipLine[0] = '\0';
         }
         
@@ -137,7 +149,7 @@ Map *mapInit(char *filename) {
             }
             auxChar = (char) fgetc(fp);    /* read next character from file */
         }
-        fgets(skipLine, 256, fp); /* make file stream point to next line) */
+        fgets(skipLine, SIZE, fp); /* make file stream point to next line) */
         skipLine[0] = '\0';
     }
 
@@ -145,6 +157,221 @@ Map *mapInit(char *filename) {
 
     return parkMap;
 }               
+
+/*
+ *  Function:
+ *      generateGraphs
+ *  Description:
+ *      generates pedestrian and car graphs with list representation
+ *
+ *  Arguments:
+ *      Pointer to struct Map
+ *  Return value:
+ *      none
+ *
+ *  Secondary effects:
+ *      initializes and computes both cGraph and pGraph fields in struct Map
+ */
+
+void buildGraphs(Map *parkMap) {
+    int n, m, p, i;    /* iteration variables */
+    int N, M ,P;
+    int x, y, z;       /* point coordinates */
+    GraphL *cGraph, *pGraph; /* adjacency list graphs */
+
+    N = parkMap->N;
+    M = parkMap->M;
+    P = parkMap->P;
+
+    /* initializing the graphs */
+    cGraph = graphInit(N * M * P);
+    pGraph = graphInit(N * M * P);
+    
+    /* start computing using the representation matrix
+     *
+     * n will go from 1 to N - 2 to ignore walls
+     * m will go from 1 to M - 2 to ignore walls
+     *
+     * later we will go over the entrance and access points
+     * to complete the graph
+     */
+
+    /* useful macros to get neighbour chars */
+    #define CENTER parkMap->mapRep[n][m][p]
+    #define LEFT parkMap->mapRep[n-1][m][p]
+    #define RIGHT parkMap->mapRep[n+1][m][p]
+    #define TOP parkMap->mapRep[n][m+1][p]
+    #define BOTTOM parkMap->mapRep[n][m-1][p]
+
+    for(n = 1; n < N - 1; n++){
+        for(m = 1; m < M - 1; m++){
+            for(p = 0; p < P; p++){
+                switch(parkMap->mapRep[n][m][p]){
+                    case '@':
+                        break;
+                    case 'x':
+                        break;
+                    case 'u':
+                        /* connect car path with upper floor */
+                        insertEdge(cGraph, toIndex(n,m,p,N,M,P),
+                                    toIndex(n,m,p + 1,N,M,P), 1);
+                        break;
+                    case 'd':
+                        /* connect car path with lower floor */
+                        insertEdge(cGraph, toIndex(n,m,p,N,M,P),
+                                    toIndex(n,m,p - 1,N,M,P), 1);
+                        break;
+
+                    case ' ':   /* if on a free way */
+
+                        /* check for possibility of edge with neighbours */
+                        /* first for the case of the car graph
+                         * then for the peon graph
+                         *
+                         * check for all directions
+                         */
+
+                        /* strchr function from <string.h> will be
+                         * used in order to check if a character 
+                         * belongs in a set of forbidden chars
+                         */
+
+                        /* LEFT */
+                        if( strchr("@exa", (int) LEFT) == NULL){
+                            insertEdge(cGraph, toIndex(n,m,p,N,M,P),
+                                    toIndex(n-1,m,p,N,M,P), 1);
+                        } 
+                        if( strchr("@xei.ud", (int) LEFT) == NULL){
+                            insertEdge(pGraph, toIndex(n,m,p,N,M,P),
+                                    toIndex(n-1,m,p,N,M,P), 1);
+                        }
+
+                        /* RIGHT */
+                        if( strchr("@exa", (int) RIGHT) == NULL){
+                            insertEdge(cGraph, toIndex(n,m,p,N,M,P),
+                                    toIndex(n+1,m,p,N,M,P), 1);
+                        } 
+                        if( strchr("@xei.ud", (int) RIGHT) == NULL){
+                            insertEdge(pGraph, toIndex(n,m,p,N,M,P),
+                                    toIndex(n+1,m,p,N,M,P), 1);
+                        }
+
+                        /* TOP */
+                        if( strchr("@exa", (int) TOP) == NULL){
+                            insertEdge(cGraph, toIndex(n,m,p,N,M,P),
+                                    toIndex(n,m+1,p,N,M,P), 1);
+                        } 
+                        if( strchr("@xei.ud", (int) TOP) == NULL){
+                            insertEdge(pGraph, toIndex(n,m,p,N,M,P),
+                                    toIndex(n,m+1,p,N,M,P), 1);
+                        }
+
+                        /* BOTTOM */
+                        if( strchr("@exa", (int) BOTTOM) == NULL){
+                            insertEdge(cGraph, toIndex(n,m,p,N,M,P),
+                                    toIndex(n,m-1,p,N,M,P), 1);
+                        } 
+                        if( strchr("@xei.ud", (int) BOTTOM) == NULL){
+                            insertEdge(pGraph, toIndex(n,m,p,N,M,P),
+                                    toIndex(n,m-1,p,N,M,P), 1);
+                        }
+                        break;
+                    case '.':
+                        /* check for possibility of edge with neighbours */
+                        /* 
+                         * only checking peon graph because no car will 
+                         * leave parking spot after parking unless is leaving
+                         *
+                         * check for all directions
+                         */
+                        
+                        /* strchr function from <string.h> will be
+                         * used in order to check if a character 
+                         * belongs in a set of forbidden chars
+                         */
+
+                        /* LEFT */
+                        if( strchr("@xei.ud", (int) LEFT) == NULL){
+                            insertEdge(pGraph, toIndex(n,m,p,N,M,P),
+                                    toIndex(n-1,m,p,N,M,P), 1);
+                        }
+
+                        /* RIGHT */
+                        if( strchr("@xei.ud", (int) RIGHT) == NULL){
+                            insertEdge(pGraph, toIndex(n,m,p,N,M,P),
+                                    toIndex(n+1,m,p,N,M,P), 1);
+                        }
+
+                        /* TOP */
+                        if( strchr("@xei.ud", (int) TOP) == NULL){
+                            insertEdge(pGraph, toIndex(n,m,p,N,M,P),
+                                    toIndex(n,m+1,p,N,M,P), 1);
+                        }
+
+                        /* BOTTOM */
+                        if( strchr("@xei.ud", (int) BOTTOM) == NULL){
+                            insertEdge(pGraph, toIndex(n,m,p,N,M,P),
+                                    toIndex(n,m-1,p,N,M,P), 1);
+                        }
+
+                }
+            }
+        }
+
+                    
+    }
+    /* end of for cycle */
+
+    /* go through entrance points and make new edges */
+    for(i = 0; i < parkMap->E; i++) {
+        x = getx(parkMap->entrancePoints[i]);
+        y = gety(parkMap->entrancePoints[i]);
+        z = getz(parkMap->entrancePoints[i]);
+
+        if(x == 0) /* at the left wall, add path to the right of the entrance */
+            insertEdge(cGraph, toIndex(x,y,z,N,M,P),
+                        toIndex(x+1,y,z,N,M,P), 1);
+        if(x == parkMap->N - 1)                 /* at the right wall ... */
+            insertEdge(cGraph, toIndex(x,y,z,N,M,P),
+                        toIndex(x-1,y,z,N,M,P), 1);
+        if(y == parkMap->M - 1)                 /* at the Top wall */
+            insertEdge(cGraph, toIndex(x,y,z,N,M,P),
+                        toIndex(x,y-1,z,N,M,P), 1);
+        if(y == 0)                              /* at the bottom wall */
+            insertEdge(cGraph, toIndex(x,y,z,N,M,P),
+                        toIndex(x,y+1,z,N,M,P), 1);
+    }
+
+    /* go through access points and make new edges */
+    for(i = 0; i < parkMap->E; i++) {
+        x = getx(parkMap->accessPoints[i]);
+        y = gety(parkMap->accessPoints[i]);
+        z = getz(parkMap->accessPoints[i]);
+
+        /* this time we connect the edge from the path to the access point
+         * since the access point does not have an edge outward but inwards
+         */
+        if(x == 0) /* at the left wall, add path to the right of the entrance */
+            insertEdge(pGraph, toIndex(x+1,y,z,N,M,P),
+                        toIndex(x,y,z,N,M,P), 1);
+        if(x == parkMap->N - 1)                 /* at the right wall ... */
+            insertEdge(pGraph, toIndex(x-1,y,z,N,M,P),
+                        toIndex(x,y,z,N,M,P), 1);
+        if(y == parkMap->M - 1)                 /* at the Top wall */
+            insertEdge(pGraph, toIndex(x,y-1,z,N,M,P),
+                        toIndex(x,y,z,N,M,P), 1);
+        if(y == 0)                              /* at the bottom wall */
+            insertEdge(pGraph, toIndex(x,y+1,z,N,M,P),
+                        toIndex(x,y,z,N,M,P), 1);
+    }
+
+    parkMap->cGraph = cGraph;
+    parkMap->pGraph = pGraph;
+
+    return;
+}
+
+
 
 /*
  *  Function:
@@ -189,6 +416,34 @@ void mapPrintStd(Map *parkMap) {
 
     return;
 }
+/*
+ *  Functions: (print graphs)
+ *
+ *  Description:
+ *      prints the desired graph previously built in parkMap
+ *
+ *  Arguments:
+ *      file stream pointer
+ *      Pointer to struct Map
+ *  Return value:
+ *      none
+ *
+ *  Secondary effects:
+ *      none
+ */
+
+void printCGraph(FILE *fp, Map *parkMap){
+    if(parkMap->cGraph!=NULL)
+        GprintToFile(fp, parkMap->cGraph);
+    return;
+}
+
+void printPGraph(FILE *fp, Map *parkMap){
+    if(parkMap->pGraph!=NULL)
+        GprintToFile(fp, parkMap->pGraph);
+    return;
+}
+
 
 /*
  *  Function:
--- parkmap.h
+++ parkmap.h
@@ -1,14 +1,60 @@
+/*
+ *  File name: parkmap.h
+ *
+ *  Author: Beatriz Ferreira & Henrique Nogueira
+ *
+ *  Description: Header file for Map structure operations. The structure
+ *          contains all extracted information from a parking station map
+ *          and this interface simple user operations over the structure
+ *               
+ *  Abstract Data Type: Map
+ *              This structure contains all required information over the
+ *          parking station's composition as given by its input file
+ *
+ *  Function list:
+ *    A) Initialization & Termination
+ *        mapInit
+ *        mapDestroy
+ *
+ *    B) Properties
+ *        ---
+ *
+ *    C) Navigation
+ *        ---
+ *
+ *    D) Lookup
+ *        ---
+ *
+ *
+ *  Dependencies:
+ *    dbg.h
+ *    point.h
+ *    stdlib.h
+ *    stdio.h
+ *    string.h
+ *
+ *  Version: 1.0
+ *
+ *  Change log: N/A
+ *
+ */
+
+/* multiple inclusion prevention */
 #ifndef __parkmap__h
 #define __parkmap__h 1
 
-#ifndef __point__h
 #include"point.h"
-#endif
+#include<stdio.h>
 
 typedef struct _map Map;
 
 Map *mapInit(char *filename);
+
+void printCGraph(FILE *fp, Map *parkMap);
+void printPGraph(FILE *fp, Map *parkMap);
+
 void mapPrintStd(Map *parkMap);
+void buildGraphs(Map *parkMap);
 
 void mapDestroy(Map *parkMap);
 
