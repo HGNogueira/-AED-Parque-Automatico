4b0a7af729b16c11bf46196a6ff773b554955b87
Henrique Nogueira <henrique.nogueira@ist.utl.pt>
2015-11-22 13:52:54 +0000

    Testing ideal path generator

--- gestor.c
+++ gestor.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 #include"parkmap.h"
+#include"point.h"
 
 int main(int argc, char* argv[]) {
     Map *parkMap;
+    Point *entrance, *access;
+    
     if(argc == 1) {
         fprintf(stderr, "Usage ...\n");
         return 1;
@@ -14,6 +17,10 @@ int main(int argc, char* argv[]) {
     buildGraphs(parkMap);
     printGraph(stdout, parkMap);
 
+    entrance = newPoint("E2", '-', 10, 0, 0);
+    access = newPoint("A2", 'C', 2, 14, 0);
+
+    findPath(parkMap, entrance, access);
 
     mapDestroy(parkMap);
 
