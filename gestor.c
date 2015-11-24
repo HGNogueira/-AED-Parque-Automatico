39a53eb183fb3932318a8c3865a6dc33f037a60f
Henrique Nogueira <henrique.nogueira@ist.utl.pt>
2015-11-24 20:24:31 +0000

    gestor now tests findPath, you can now write entrance ID and access descriptor and the program will find an ideal path

--- gestor.c
+++ gestor.c
@@ -2,12 +2,16 @@
 #include"parkmap.h"
 #include"point.h"
 
+void Usage(){
+    fprintf(stderr, "Usage:\n\t./gestor <parque.cfg> <entranceID> <accessDesc>\n");
+    return;
+}
+
 int main(int argc, char* argv[]) {
     Map *parkMap;
-    Point *entrance, *access;
     
-    if(argc == 1) {
-        fprintf(stderr, "Usage ...\n");
+    if(argc < 4) {
+        Usage();
         return 1;
     }
     parkMap = mapInit(argv[1]);
@@ -15,16 +19,10 @@ int main(int argc, char* argv[]) {
     mapPrintStd(parkMap);
 
     buildGraphs(parkMap);
-    printGraph(stdout, parkMap);
 
-    entrance = newPoint("E2", '-', 10, 0, 0);
-    access = newPoint("A2", 'C', 2, 14, 0);
-
-    findPath(parkMap, entrance, access);
+    findPath(parkMap, argv[2], argv[3][0]);
 
     mapDestroy(parkMap);
-    pointDestroy(entrance);
-    pointDestroy(access);
 
     return 0;
 }
